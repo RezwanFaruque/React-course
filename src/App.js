0a3ed58197c4a56e14ed9ccf35f4770be04ecd30
Muntaha Ibn Ahmed <muntaha.techlogicians@gmail.com>
2020-09-26 02:57:21 +0600

    components one

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './Components/menuComponents';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
-        <div className="conatainer">
+        <div className="container">
           <NavbarBrand href="/">Hello</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
        
     </div>
   );
